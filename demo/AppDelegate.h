a31fb49988fcffc7404ef5aec6ef55abec403b84
jiaj <jiaj@telsafe.com.cn>
2018-09-22 22:31:08 +0800

    a

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 贾俊 on 2018/9/20.
+//  Copyright © 2018年 贾俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by 贾俊 on 2018/9/20.
+//  Copyright © 2018年 贾俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
